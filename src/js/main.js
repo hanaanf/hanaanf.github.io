f0b7c9e4dc4327ca45e9cfe5043fd21f55c492e5
Feroz Abdul Rehman <feroz.rehman@salesforce.com>
2020-04-19 14:38:09 +0100

    first stage is done !!!! :)

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,16 @@ function changecolormove() {
 function changecolorx2() {
     document.querySelector("h1").style.color = "green";
 }
+function mark() {
+    let character = document.querySelector('character');
+    let total = 5;
+    let score = 0;
+    if (document.querySelector('#numbox').value==(7+5)) {
+        character.className = "pos-first-right col-9";
+        score++;
+    } else {
+        character.className = "pos-first-wrong col-9";
+        score = score==0 ? 0 : score--;
+    }
+    document.querySelector("results").innerHTML = score + "/" + total;
+}
